b0d83ae7cae419d8e92e0cc1376677d50ac7c313
vishal pathak <vishalpathak842@gmail.com>
2025-04-01 12:42:46 +0530

    add authentication routes to server configuration

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb.js";
+import authRouter from "./routes/auth.routes.js";
 
 const app = express();
 
@@ -13,6 +14,8 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ credentials: true }));
 
+// API EndPoints
 app.get("/", (req, res) => res.send("API Working"));
+app.use('/api/auth', authRouter)
 
 app.listen(port, () => console.log(`Server started on PORT : ${port}`));
