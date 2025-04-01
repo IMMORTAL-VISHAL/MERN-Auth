b3c416dece872d441e70a107f033364c6ea09a48
vishal pathak <vishalpathak842@gmail.com>
2025-04-01 14:10:07 +0530

    add route for sending verification OTP with user authentication

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,10 +1,17 @@
 import express from "express";
-import { login, logout, register } from "../controllers/auth.controller.js";
+import {
+  login,
+  logout,
+  register,
+  sendVerifyOtp,
+} from "../controllers/auth.controller.js";
+import userAuth from "../middleware/userAuth.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/register", register);
 authRouter.post("/login", login);
 authRouter.post("/logout", logout);
+authRouter.post("/send-verify-otp", userAuth, sendVerifyOtp);
 
 export default authRouter;
