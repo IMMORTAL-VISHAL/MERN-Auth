72b0b33902a45112867645f6052da0d58f64aa9a
vishal pathak <vishalpathak842@gmail.com>
2025-04-02 08:40:50 +0530

    Add user routes integration in server configuration

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import "dotenv/config";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/mongodb.js";
 import authRouter from "./routes/auth.routes.js";
+import userRouter from "./routes/user.routes.js";
 
 const app = express();
 
@@ -17,5 +18,6 @@ app.use(cors({ credentials: true }));
 // API EndPoints
 app.get("/", (req, res) => res.send("API Working"));
 app.use('/api/auth', authRouter)
+app.use('/api/user',userRouter)
 
 app.listen(port, () => console.log(`Server started on PORT : ${port}`));
