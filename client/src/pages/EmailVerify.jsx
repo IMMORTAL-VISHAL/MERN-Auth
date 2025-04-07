5ae98c1f82082a7d5b4fd61eaa5fd25a0a6f28f2
vishal pathak <vishalpathak842@gmail.com>
2025-04-07 23:37:25 +0530

    Fix formatting in EmailVerify component for consistency

--- client/src/pages/EmailVerify.jsx
+++ client/src/pages/EmailVerify.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const EmailVerify = () => {
   return (
     <div>
-        <h1> Email Verify</h1>
+      <h1>Email Verify</h1>
     </div>
-  )
-}
+  );
+};
 
-export default EmailVerify
+export default EmailVerify;
